45440eef92c68a0d71c9877864f562a790bbf2e5
Vinicius Calgaro <vcalgaro@sweda.com.br>
2025-11-19 14:19:02 -0300

    refator: Aprimora navegação e ícones da `BottomNavigationBar`

--- app/src/main/java/com/vinicalgaro/cidarte/presentation/components/CidarteBottomNavigationBar.kt
+++ app/src/main/java/com/vinicalgaro/cidarte/presentation/components/CidarteBottomNavigationBar.kt
@@ -1,9 +1,16 @@
 package com.vinicalgaro.cidarte.presentation.components
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.vinicalgaro.cidarte.presentation.navigation.BottomNavItem
@@ -16,14 +23,27 @@ fun CidarteBottomNavigationBar(navController: NavController) {
         BottomNavItem.Settings
     )
 
-    NavigationBar {
+    NavigationBar(
+        modifier = Modifier.height(82.dp)
+    ) {
         val navBackStackEntry = navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry.value?.destination?.route
 
         items.forEach { item ->
+            val isSelected = currentRoute == item.route
+
             NavigationBarItem(
-                icon = { Icon(imageVector = item.icon, contentDescription = item.route) },
+                icon = {
+                    Icon(
+                        imageVector = if (isSelected) item.selectedIcon else item.unselectedIcon,
+                        contentDescription = item.route,
+                        modifier = Modifier.size(32.dp)
+                    )
+                },
                 selected = currentRoute == item.route,
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.Transparent,
+                ),
                 onClick = {
                     if (currentRoute != item.route) {
                         navController.navigate(item.route) {
--- app/src/main/java/com/vinicalgaro/cidarte/presentation/navigation/BottomNavItem.kt
+++ app/src/main/java/com/vinicalgaro/cidarte/presentation/navigation/BottomNavItem.kt
@@ -2,12 +2,22 @@ package com.vinicalgaro.cidarte.presentation.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.SavedSearch
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class BottomNavItem(val route: String, val icon: ImageVector) {
-    object Home : BottomNavItem(Destinations.HOME_ROUTE, Icons.Default.Home)
-    object Search : BottomNavItem(Destinations.SEARCH_ROUTE, Icons.Default.Search)
-    object Settings : BottomNavItem(Destinations.SETTINGS_ROUTE, Icons.Default.Settings)
+sealed class BottomNavItem(
+    val route: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+) {
+    object Home :
+        BottomNavItem(Destinations.HOME_ROUTE, Icons.Filled.Home, Icons.Outlined.Home)
+    object Search :
+        BottomNavItem(Destinations.SEARCH_ROUTE, Icons.Filled.SavedSearch, Icons.Outlined.Search)
+    object Settings :
+        BottomNavItem(Destinations.SETTINGS_ROUTE, Icons.Filled.Settings, Icons.Outlined.Settings)
 }
